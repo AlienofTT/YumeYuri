7ae90f4217f46e0f44391947d280247ca90b5ca5
不知 <Alienoftt@gmail.com>
2020-09-15 16:43:32 -0400

    models update

--- Reader/models.py
+++ Reader/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Manga(models.Model):
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=200)
+
+
+class Chapter(models.Model):
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=200)
+	manga = models.ForeignKey('Manga', on_delete=models.CASCADE)
+
+
+class Image(models.Model):
+	id = models.AutoField(primary_key=True)
+	fileName = models.CharField(max_length=100)
+	chapter = models.ForeignKey('Chapter', on_delete=models.CASCADE)
--- YumeYuri/settings.py
+++ YumeYuri/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Reader.apps.ReaderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
