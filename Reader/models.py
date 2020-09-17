2c14b93f7ee8cf136b10e1e492e39aa13d7f5980
不知 <Alienoftt@gmail.com>
2020-09-17 18:23:11 -0400

    Update models.py

--- Reader/models.py
+++ Reader/models.py
@@ -5,15 +5,17 @@ from django.db import models
 class Manga(models.Model):
 	id = models.AutoField(primary_key=True)
 	title = models.CharField(max_length=200)
+	bgmId = models.IntegerField(null=True)
+	dmzjId = models.CharField(max_length=100)
 
 
 class Chapter(models.Model):
 	id = models.AutoField(primary_key=True)
 	title = models.CharField(max_length=200)
-	manga = models.ForeignKey('Manga', on_delete=models.CASCADE)
+	manga = models.ForeignKey(Manga, on_delete=models.CASCADE)
 
 
 class Image(models.Model):
 	id = models.AutoField(primary_key=True)
 	fileName = models.CharField(max_length=100)
-	chapter = models.ForeignKey('Chapter', on_delete=models.CASCADE)
+	chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
